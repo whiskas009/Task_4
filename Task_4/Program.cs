aca1f42135f380123b60c2c868a5db9603b0918c
whiskas009 <76867818+whiskas009@users.noreply.github.com>
2022-05-12 20:15:47 +0300

    #1

---
+++ Task_4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите кол-во картинок");
+            int pictures = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите кол-во картинок в одном ряду");
+            int picturesInRow = int.Parse(Console.ReadLine());
+
+            int fullRows = pictures / picturesInRow;
+            double picturesNotContained = pictures % fullRows;
+
+            Console.WriteLine($"Количество полных рядов составляет: {fullRows}\nКоличество невместившихся картинок: {picturesNotContained}");
+        }
+    }
+}
